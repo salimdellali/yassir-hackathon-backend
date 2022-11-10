aae8dfa10a30723d9b082d8f1e7b62f855ac0bf5
Salim DELLALI <dellali.salim@gmail.com>
2022-11-10 13:31:29 +0100

    enabled cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import { ClientRouter } from './modules/client/'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 const API_VERSION = '/api/v1'
 
+app.use(cors())
 app.use(express.json())
 
 app.use(API_VERSION + '/client', ClientRouter)
